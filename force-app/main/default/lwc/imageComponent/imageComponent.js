779b3c52e6a71c01e39c7cc2083b494de2452a3c
jayesh-github215 <137868091+jayesh-github215@users.noreply.github.com>
2023-10-12 12:36:45 +0530

    Delete force-app/main/default/lwc directory

--- force-app/main/default/lwc/imageComponent/imageComponent.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-import logo from '@salesforce/resourceUrl/ResImage';
-
-export default class ImageComponent extends LightningElement {
-    imageURL = logo;
-}
